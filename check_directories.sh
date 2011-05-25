f11183614b3698f450cba220731d34ba7a34b158
Jan Harasym <dijit@fsfe.org>
2011-05-25 13:14:55 +0100

    [*] Created a easier to edit flow. [*] Stripped Trailing slashes from directories (Which led to a bug in creating checksum files)

--- check_directories.sh
+++ check_directories.sh
@@ -4,10 +4,11 @@
 ##
 # Usage: ... I KNOW THE USAGE IT'S IN MY HEAD.--- GO AWAY!
 ## script-name.sh -w sitename (must be defined in the rootdir path)
-rootdir="./"; ## Line to edit, most used directory for users requiring hashing,| set to "" to force absolute paths.
+rootdir="./"; 			 ## Line to edit, most used directory for users requiring hashing,| set to "" to force absolute paths.
+site=`echo $2 | sed 's/\/$//g'`; ## Casting is required for it to be used in functions. (IE; dircheck) | set $2 to any folder you require for static paths.
 
+## Do not edit below here unless you know what you are doing.
 usage="\e[04;01mTo check the contents of web dir:\n\t\e[00;00m$0 -c \e[07;01msite\e[00;00m\e[04;01m\nTo reenumerate contents of web dir:\n\t\e[00;00m$0 -w \e[07;01msite\e[00;00m";
-site=$2; #casting is required for it to be used in functions. (IE; dircheck)
 
 success() {
      echo -e "\t\t\t\t\t\t\e[00;00m[\e[00;32m  OK  \e[00;00m]"; # so many tabulations! D:
@@ -30,18 +31,17 @@ fi
 # I mostly just waste time.
 case $1 in
   --check|-c) dircheck; # .. when I should be commenting that is.
-  run="`md5sum -c .checksums_$2.md5 | grep FAILED`";
-  printf "Checking Files:\t" ;;
+  printf "Checking Files:\t" ;
+  run="`md5sum -c --quiet .checksums_$site.md5`";;
   --write|-w) dircheck;
-  run="`find $rootdir$2/ -type f -print0 | xargs -0 md5sum > .checksums_$2.md5`";
+  run="`find $rootdir$2/ -type f -print0 | xargs -0 md5sum > .checksums_$site.md5`";
   printf "Enumerating Files:" ;;
   *) echo -e $usage;
      other="1" ;;
 esac
-if [ "$other" == "" ] ; then
-  if [ "$run" != "" ] ; then
-     fail;  # Did you know...
-     exit 1;  # ...I was...
+if [ -z $other ] ; then
+  if [ -n "$run" ] ; then  # Did you know...
+     fail;  #  ... I was ...
   else	#  ...Getting blown under the desk while writing this? :P
      success; # I Just thought I'd leave you with that thought.
      exit 0;
